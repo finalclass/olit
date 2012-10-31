0bb896788c449cdc579358503429bc01ca6aa552
Szymon Wygnański <s@finalclass.net>
2012-10-31 19:12:09 +0100

    Added Firefox and opera

--- testacular.conf.js
+++ testacular.conf.js
@@ -60,7 +60,7 @@ autoWatch = true;
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-browsers = ['Chrome'];
+browsers = ['Firefox', 'Opera', 'Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
 // CLI --capture-timeout 5000
